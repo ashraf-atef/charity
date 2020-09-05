a9fdfde5e28533f38dde3a9ecfa86b1e03b600e9
ashraf <ashraf1abdelrasool@gmail.com>
2020-09-05 12:37:38 +0200

    change endpoint

--- src/main/java/org/revo/charity/Domain/User.java
+++ src/main/java/org/revo/charity/Domain/User.java
@@ -2,6 +2,8 @@ package org.revo.charity.Domain;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.Fetch;
+import org.hibernate.annotations.FetchMode;
 
 import javax.persistence.*;
 import java.util.HashSet;
@@ -17,5 +19,6 @@ public class User {
     private Long id;
     private String name;
     @OneToMany
+    @Fetch(FetchMode.SUBSELECT)
     private Set<Phone> phones = new HashSet<>();
 }
