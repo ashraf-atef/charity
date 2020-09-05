e28f6f13611811b171a3400c407a3cc4c36dfc0d
ashraf <ashraf1abdelrasool@gmail.com>
2020-09-05 12:24:49 +0200

    change endpoint

--- src/main/java/org/revo/charity/Domain/User.java
+++ src/main/java/org/revo/charity/Domain/User.java
@@ -16,6 +16,6 @@ public class User {
     @GeneratedValue
     private Long id;
     private String name;
-    @OneToMany(mappedBy = "user",fetch = FetchType.LAZY)
+    @OneToMany
     private List<Phone> phones = new ArrayList<>();
 }
