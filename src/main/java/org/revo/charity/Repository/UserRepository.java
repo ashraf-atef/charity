9ab722aefc6be13be4d8e1126ef6850b47131774
Ashraf-Atef <ashrafatef84@gmail.com>
2020-10-07 11:30:50 +0200

    Try a funny example with revo :D

--- src/main/java/org/revo/charity/Repository/UserRepository.java
+++ src/main/java/org/revo/charity/Repository/UserRepository.java
@@ -1,10 +1,21 @@
 package org.revo.charity.Repository;
 
 import org.revo.charity.Domain.User;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends CrudRepository<User, Long> {
     Optional<User> findByEmail(String email);
+
+    @Query(value = "select user from User user where user.email like '%:query%'")
+    List<User> selectInArea(@Param("query") String query);
+
+    @Query(value = "select * from r_user where email like :query", nativeQuery =true)
+    Optional<User> findRevo(@Param("query") String query);
+
+    List<User> findAllByEmailLike(String query);
 }
